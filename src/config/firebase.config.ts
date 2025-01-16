a29d721fbdecae392277ca775344a8ded1f9f966
Abimael <abimael.spdev@gmail.com>
2025-01-16 08:12:37 -0400

    chore: add firebase configuration

---
+++ src/config/firebase.config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzTKrPiXSWa8Hjeb8u2P0GeH6yOIB8DoE",
+  authDomain: "ecommerce-fsc.firebaseapp.com",
+  projectId: "ecommerce-fsc",
+  storageBucket: "ecommerce-fsc.firebasestorage.app",
+  messagingSenderId: "184467831175",
+  appId: "1:184467831175:web:67826ae07327d6e7f2fb22"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
