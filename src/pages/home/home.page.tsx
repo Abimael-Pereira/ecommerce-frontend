4610075050868c47a6eb4de0c488ab98ac1022b2
Abimael <abimael.spdev@gmail.com>
2025-01-16 22:46:26 -0400

    style: change Home Page div to Fragment

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,14 +1,14 @@
 // Components
-import Categories from "../../components/categories/categories.components";
-import Header from "../../components/header/header.component";
+import Categories from '../../components/categories/categories.components';
+import Header from '../../components/header/header.component';
 
 const HomePage = () => {
-    return ( 
-        <div>
-            <Header />
-            <Categories/>
-        </div>
-     );
-}
- 
+  return (
+    <>
+      <Header />
+      <Categories />
+    </>
+  );
+};
+
 export default HomePage;
