08242ad33d37814a3650645888ff0a1b2a50d554
Abimael <abimael.spdev@gmail.com>
2025-01-17 14:32:34 -0400

    feat: add CustomButton to Login Page

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,4 +1,9 @@
+import { BsGoogle } from 'react-icons/bs';
+import { FiLogIn } from 'react-icons/fi';
+
+import CustomButton from '../../components/custom-buttom/custom-button.component';
 import Header from '../../components/header/header.component';
+
 import {
   LoginContainer,
   LoginContent,
@@ -15,11 +20,17 @@ const LoginPage = () => {
       <LoginContainer>
         <LoginContent>
           <LoginHeadLine>Entre com a sua conta!</LoginHeadLine>
-            
+
+          <CustomButton startIcon={<BsGoogle size={18} />}>
+            Entrar com o Google
+          </CustomButton>
+
           <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
 
           <LoginInputContainer></LoginInputContainer>
           <LoginInputContainer></LoginInputContainer>
+
+          <CustomButton startIcon={<FiLogIn size={18} />}>Entrar</CustomButton>
         </LoginContent>
       </LoginContainer>
     </>
