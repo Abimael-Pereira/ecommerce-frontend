0d40e4d5484a6aa73ce088793d2499fc92d0d742
Abimael <abimael.spdev@gmail.com>
2025-01-16 23:13:34 -0400

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: '#212529',
+  text: {
+    white: '#F8F9F1',
+    dark: '#343A40',
+  },
+  background: {
+    dark: '#212529',
+    white: '#F8F9FA',
+  },
+  input: {
+    placeholder: '#6C757D',
+    background: '#E9ECEF',
+  },
+  error: '#FF6A6A',
+};
+
+export default Colors;
