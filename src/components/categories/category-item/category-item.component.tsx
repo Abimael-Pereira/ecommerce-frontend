208c57fbb77436c395f9ff96d5c3f3d0c27710f8
Abimael <abimael.spdev@gmail.com>
2025-01-16 22:47:59 -0400

    fix: upgrade Category Item background image

--- src/components/categories/category-item/category-item.component.tsx
+++ src/components/categories/category-item/category-item.component.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from 'react';
 
 import Category from '../../../types/category.types';
 
-import './category-item.styles.scss';
+import './category-item.styles.css';
 
 interface CategoryItemProps {
   category: Category;
@@ -12,7 +12,7 @@ const CategoryItem: FunctionComponent<CategoryItemProps> = ({ category }) => {
   return (
     <div
       className="category-item-container"
-      style={{ backgroundImage: category.imageUrl }}
+      style={{ backgroundImage: `url('${category.imageUrl}')` }}
     >
       <div className="category-name">
         <p>{category.displayName}</p>
