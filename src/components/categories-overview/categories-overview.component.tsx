90fc26756f34fb5a89f54ea91c5b851598bc9aae
Abimael <abimael.spdev@gmail.com>
2025-02-10 23:48:22 -0400

    feat: add Category Overview Component

--- src/components/categories-overview/categories-overview.component.tsx
+++ src/components/categories-overview/categories-overview.component.tsx
@@ -3,6 +3,7 @@ import { useContext, useEffect } from 'react';
 import { Container } from './categories-overview.styles';
 
 import CategoryContext from '../../contexts/category.context';
+import CategoryOverview from '../category-overview/category-overview.component';
 
 
 const CategoriesOverview = () => {
@@ -17,9 +18,7 @@ const CategoriesOverview = () => {
   return (
     <Container>
       {categories.map((category) => (
-        <div key={category.id}>
-          <h1>{category.name}</h1>
-        </div>
+        <CategoryOverview key={category.id} category={category} />
       ))}
     </Container>
   );
