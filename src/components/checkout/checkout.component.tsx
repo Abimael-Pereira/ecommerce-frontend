489b130ce6f21c7568d54b2fc6d698fc9a19e81b
Abimael <abimael.spdev@gmail.com>
2025-02-16 19:29:02 -0400

    feat: add stripe api integration to checkout

--- src/components/checkout/checkout.component.tsx
+++ src/components/checkout/checkout.component.tsx
@@ -1,5 +1,7 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { BsBagCheck } from 'react-icons/bs';
+import axios from 'axios';
+
 import CartContext from '../../contexts/cart.context';
 
 import {
@@ -11,31 +13,59 @@ import {
 
 import CustomButton from '../custom-buttom/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import LoadingComponent from '../loading/loading.component';
 
 const Checkout = () => {
   const { products, productsTotalPrice } = useContext(CartContext);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleFinishPurchaseClick = async () => {
+    setIsLoading(true);
+    try {
+      const { data } = await axios.post(
+        `${import.meta.env.VITE_REACT_APP_API_URL}/create-checkout-session`,
+        {
+          products,
+        },
+      );
+
+      window.location.href = data.url;
+    } catch (error) {
+      console.error('Error while trying to finish purchase:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <CheckoutContainer>
-      <CheckoutTitle>Checkout</CheckoutTitle>
+    <>
+      {isLoading && <LoadingComponent />}
+      <CheckoutContainer>
+        <CheckoutTitle>Checkout</CheckoutTitle>
 
-      {products.length > 0 ? (
-        <>
-          <CheckoutProducts>
-            {products.map((product) => (
-              <CartItem key={product.id} product={product} />
-            ))}
-          </CheckoutProducts>
+        {products.length > 0 ? (
+          <>
+            <CheckoutProducts>
+              {products.map((product) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+            </CheckoutProducts>
 
-          <CheckoutTotal>R${productsTotalPrice}</CheckoutTotal>
+            <CheckoutTotal>R${productsTotalPrice}</CheckoutTotal>
 
-          <CustomButton startIcon={<BsBagCheck />}>
-            Finalizar compra
-          </CustomButton>
-        </>
-      ) : (
-        <p>Seu carrinho está vazio!</p>
-      )}
-    </CheckoutContainer>
+            <CustomButton
+              startIcon={<BsBagCheck />}
+              onClick={handleFinishPurchaseClick}
+            >
+              Finalizar compra
+            </CustomButton>
+          </>
+        ) : (
+          <p>Seu carrinho está vazio!</p>
+        )}
+      </CheckoutContainer>
+    </>
   );
 };
 
