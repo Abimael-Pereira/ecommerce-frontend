9453f4a118e7329ed698e9eac9caa47f94476ee0
Abimael <abimael.spdev@gmail.com>
2025-02-26 14:54:30 -0400

    test: ensure cart products count are correctly shown on header

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -5,6 +5,8 @@ import Header from './header.component';
 
 import UserContext from '../../contexts/user.context';
 import User from '../../types/user.types';
+import CartProduct from '../../types/cart.types';
+import CartContext from '../../contexts/cart.context';
 
 const userTest: User = {
   id: '1',
@@ -14,6 +16,23 @@ const userTest: User = {
   provider: 'firebase',
 };
 
+const productsTest: CartProduct[] = [
+  {
+    id: '1',
+    name: 'Product 1',
+    price: 10,
+    quantity: 3,
+    imageUrl: 'https://test.com/image.png',
+  },
+  {
+    id: '2',
+    name: 'Product 2',
+    price: 20,
+    quantity: 2,
+    imageUrl: 'https://test.com/image.png',
+  },
+];
+
 const createUserContextValue = (isAuthenticated = false) => ({
   isAuthenticated,
   currentUser: isAuthenticated ? userTest : null,
@@ -21,11 +40,35 @@ const createUserContextValue = (isAuthenticated = false) => ({
   logoutUser: jest.fn(),
 });
 
-const HeaderWithContexts = ({ isAuthenticated = false }) => {
+const createCartContextValue = (products: CartProduct[] = []) => {
+  const toggleCart = jest.fn();
+  return {
+    isVisible: false,
+    productsTotalPrice: products.reduce(
+      (acc, product) => acc + product.price * product.quantity,
+      0,
+    ),
+    productsCount: products.reduce((acc, product) => acc + product.quantity, 0),
+    products: products,
+    addProductToCart: jest.fn(),
+    toggleCart: toggleCart,
+    removeProductFromCart: jest.fn(),
+    increaseProductQuantity: jest.fn(),
+    decreaseProductQuantity: jest.fn(),
+    clearCart: jest.fn(),
+  };
+};
+
+const HeaderWithContexts = ({
+  isAuthenticated = false,
+  products = [] as CartProduct[],
+}) => {
   return (
     <BrowserRouter>
       <UserContext.Provider value={createUserContextValue(isAuthenticated)}>
-        <Header />
+        <CartContext.Provider value={createCartContextValue(products)}>
+          <Header />
+        </CartContext.Provider>
       </UserContext.Provider>
     </BrowserRouter>
   );
@@ -43,5 +86,11 @@ describe('Header', () => {
 
     expect(screen.getByText('Login')).toBeInTheDocument();
     expect(screen.getByText('Criar conta')).toBeInTheDocument();
+  });
+
+  it('should show correct cart products count', () => {
+    render(<HeaderWithContexts products={productsTest} />);
+
+    expect(screen.getByText('5')).toBeInTheDocument();
   });
 });
