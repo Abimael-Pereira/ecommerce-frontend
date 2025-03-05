0e39408dc808992af2938fb987c95586356551f3
Abimael <abimael.spdev@gmail.com>
2025-03-05 01:40:59 -0400

    test: ensure categories fetches and shows categories

---
+++ src/components/categories/categories.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from '@testing-library/react';
+import * as firestore from 'firebase/firestore';
+import Categories from './categories.components';
+import { BrowserRouter } from 'react-router-dom';
+
+jest.mock('firebase/firestore');
+
+describe('Categories', () => {
+  it('should fetch and show categories', async () => {
+    const mockedFirestore = firestore as any;
+
+    mockedFirestore.getDocs.mockReturnValue([
+      {
+        data() {
+          return {
+            id: '1',
+            displayName: 'Category 1',
+          };
+        },
+      },
+    ]);
+
+    mockedFirestore.collection.mockReturnValue({
+      withConverter: () => {},
+    });
+
+    render(
+      <BrowserRouter>
+        <Categories />
+      </BrowserRouter>,
+    );
+
+    await expect(screen.findByText('Category 1')).toBeTruthy();
+  });
+});
