4cd98bcebec196da65fb3750d6f3de3ad8fd809d
Abimael <abimael.spdev@gmail.com>
2025-03-05 23:17:22 -0400

    test: ensure sign up shows error if email is invalid

--- src/pages/sign-up/sign-up.test.tsx
+++ src/pages/sign-up/sign-up.test.tsx
@@ -13,12 +13,30 @@ describe('Sign Up', () => {
 
     const submitButton = screen.getByRole('button', { name: 'Criar conta' });
 
-    userEvent.click(submitButton);
+    await userEvent.click(submitButton);
 
     await screen.findByText('Nome é obrigatório');
     await screen.findByText('Sobrenome é obrigatório');
     await screen.findByText('E-mail é obrigatório');
     await screen.findByText('Senha é obrigatória');
     await screen.findByText('Confirmação de senha é obrigatória');
+  });
+
+  it('should show error when filling an invalid email', async () => {
+    render(
+      <BrowserRouter>
+        <SignUpPage />
+      </BrowserRouter>,
+    );
+
+    const emailInput = screen.getByPlaceholderText('Digite seu e-mail');
+
+    await userEvent.type(emailInput, 'invalid-email');
+
+    const submitButton = screen.getByRole('button', { name: 'Criar conta' });
+
+    await userEvent.click(submitButton);
+
+    await screen.findByText('Insira um e-mail válido');
   });
 });
