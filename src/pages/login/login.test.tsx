c1b932c9677f120af324134a87046c3ae3a95fbe
Abimael <abimael.spdev@gmail.com>
2025-03-05 22:58:39 -0400

    test: ensure login shows error if email is invalid

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -17,4 +17,20 @@ describe('Login', () => {
     expect(screen.getByText(/e-mail é obrigatório/i)).toBeInTheDocument();
     expect(screen.getByText(/senha é obrigatória/i)).toBeInTheDocument();
   });
+
+  it('should show error if email is invalid', async () => {
+    render(
+      <BrowserRouter>
+        <LoginPage />
+      </BrowserRouter>,
+    );
+
+    const emailInput = screen.getByPlaceholderText(/digite seu e-mail/i);
+    userEvent.type(emailInput, 'invalid-email');
+
+    const loginButton = screen.getByTestId('login-submit');
+    await userEvent.click(loginButton);
+
+    expect(screen.getByText(/insira um e-mail válido/i)).toBeInTheDocument();
+  });
 });
